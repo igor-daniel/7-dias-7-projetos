2b1bd464e7d2b42da69024e956421bd08c6a7422
Igor Daniel da Silva <44319834+igor-daniel@users.noreply.github.com>
2022-08-21 15:22:50 -0300

    Projeto clima pronto

--- Clima/script.js
+++ Clima/script.js
@@ -3,6 +3,7 @@ document.querySelector('.busca').addEventListener('submit', async (event)=>{
   let input = document.querySelector('#searchInput').value;
 
   if(input !== ''){
+    clearInfo();
     showWaring('Carregando...')
 
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=d06cdb298fafc83c520d5ab677fc477e&units=metric&lang=pt_br`
@@ -10,12 +11,42 @@ document.querySelector('.busca').addEventListener('submit', async (event)=>{
     let results = await fetch(url);
     let json = await results.json();
 
-    console.log(json);
+    if(json.cod === 200){
+      showInfo({
+        name: json.name,
+        country: json.sys.country,
+        temp: json.main.temp,
+        tempIcon: json.weather[0].icon,
+        windSpeed: json.wind.speed,
+        windAngle: json.wind.deg
+      })
+    }else{
+      clearInfo();
+      showWaring('Não encontramos essa cidade.')
+    }
 
   }else{
-    showWaring('Digite uma cidade')
+    clearInfo();
   }
 })
+function showInfo(json){
+  showWaring('');
+  
+  document.querySelector('.titulo').innerHTML = `${json.name}, ${json.country}`;
+  document.querySelector('.tempInfo').innerHTML = `${json.temp} <sup>°C</sup>`;
+  document.querySelector('.ventoInfo').innerHTML = `${json.windSpeed} <span>km/h</span>`
+  
+  document.querySelector('.temp img').setAttribute('src', `http://openweathermap.org/img/wn/${json.tempIcon}@2x.png`)
+
+  document.querySelector('.ventoPonto').style.transform = `rotate(${json.windAngle-90}deg)`;
+  document.querySelector(".resultado").style.display = 'block';
+
+}
+
+function clearInfo(){
+  showWaring('');
+  document.querySelector('.resultado').style.display = 'none'
+}
 
 function showWaring(msg){
   document.querySelector('.aviso').innerHTML = msg
